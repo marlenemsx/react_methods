f4f9d9b6e979517668d41da96b1dcbd13122297e
Marlene Maisonniaux <design.msx@gmail.com>
2018-05-30 10:16:11 +0200

    quete router

---
+++ router-react/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+// import des elements et des composants
+import { Route, NavLink } from "react-router-dom";
+
+import "./App.css";
+import Home from "./Home";
+import History from "./History";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Quête React router</h1>
+        </header>
+        {/* link des routes */}
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "red"
+              }}
+              exact
+            >
+              Home
+            </NavLink>
+          </li>
+          <li onclick="myFunction()">
+            <NavLink
+              to="/notre-history"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "red"
+              }}
+              exact
+            >
+              History
+            </NavLink>
+          </li>
+        </ul>
+
+        {/*********** mes routes **************/}
+        <Route exact path="/" component={Home} />
+        <Route path="/notre-history" component={History} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ router-react/src/History.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class History extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h2>Notre histoire</h2>;
+  }
+}
+
+export default History;
